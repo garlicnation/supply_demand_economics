25b2d5888ff6a0458e0f1b7a00b0139afdb689b3
garlicnation <ajo@gsf.org>
2011-10-05 14:25:04 -0700

    Added more debugging statements

--- simpy_tutorials/MachRep1.py
+++ simpy_tutorials/MachRep1.py
@@ -7,21 +7,27 @@ class G:
 class MachineClass(SimPy.Simulation.Process):
     UpRate = 1/1.0
     RepairRate = 1/0.5
+
     TotalUpTime = 0.0
     NextID = 0
-    def __init__(self):
+    def __init__(self, StartUpTime=0.0):
         SimPy.Simulation.Process.__init__(self)
         
-        self.StartUpTime = 0.0
+        self.StartUpTime = StartUpTime
         self.ID = MachineClass.NextID
         MachineClass.NextID += 1
         
     def Run(self):
+        RepairTime = 0.0
         while True:
             self.StartUpTime = SimPy.Simulation.now()
             
+            print "T= %8.2f\tMachine %d working. Took %f to repair" % (SimPy.Simulation.now(),self.ID, RepairTime)
+
             UpTime = G.Rnd.expovariate(MachineClass.UpRate)
             yield SimPy.Simulation.hold, self, UpTime
+
+            print "T= %8.2f\tMachine %d broke down after %f minutes" % (SimPy.Simulation.now(),self.ID, UpTime)
             
             MachineClass.TotalUpTime += SimPy.Simulation.now() - self.StartUpTime
             RepairTime = G.Rnd.expovariate(MachineClass.RepairRate)
@@ -37,7 +43,7 @@ def main():
         M = MachineClass()
         SimPy.Simulation.activate(M, M.Run())
         
-    MaxSimtime = 10000.0
+    MaxSimtime = 10.0
     SimPy.Simulation.simulate(until=MaxSimtime)
     print "the percentage of up time was", MachineClass.TotalUpTime/(machines*MaxSimtime)
     
