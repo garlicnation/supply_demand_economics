a67e421b77c6a8954a84620878a40453a4abc736
AJ Ortega <ajo@gsf.org>
2011-11-04 09:56:58 -0700

    Bugfix

--- rule_agents/agents.py
+++ rule_agents/agents.py
@@ -6,7 +6,7 @@ def Agent(object):
         self.actions = {}
         self.state = {'energy':100, 'money':100, 'time':0, 'capabilities':[], 'materials':[]}
         for action in actions:
-            self.actions["%s%d" % (action(self.state), actionID)]
+            self.actions["%s%d" % (action(self.state), actionID)] = action
             actionID += 1
         
         """An agent's state is the collection of keys and values that 
